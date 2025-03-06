0bffacd61b082336b2f6f2cb439f0d7fe952e7c4
PhalyaJ <phalyajhamb@PhalyaJs-MacBook-Air.local>
2025-03-06 09:46:45 +0530

    Add routes

---
+++ routes/reviewRoutes.js
@@ -0,0 +1,8 @@
+const express = require("express");
+const router = express.Router();
+//const reviewController = require("../controllers/reviewController");
+
+//router.post("/:productId/review", reviewController.createReview);
+//router.get("/:productId/reviews", reviewController.getProductReviews);
+
+module.exports = router;

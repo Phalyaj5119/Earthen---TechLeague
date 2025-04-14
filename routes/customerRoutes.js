6292f7cebf56bc1e18c3f30818339c365c1a9db7
PhalyaJ <phalyajhamb@PhalyaJs-MacBook-Air.local>
2025-04-14 13:10:36 +0530

    Deleetd Previous  routes

--- routes/customerRoutes.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const customerController = require("../controllers/customerController");
-
-router.post("/register", customerController.registerCustomer);
-router.post("/login", customerController.loginCustomer);
-
-module.exports = router;
--- routes/reviewRoutes.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const router = express.Router();
-//const reviewController = require("../controllers/reviewController");
-
-//router.post("/:productId/review", reviewController.createReview);
-//router.get("/:productId/reviews", reviewController.getProductReviews);
-
-module.exports = router;
